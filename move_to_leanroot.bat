be9ec66524b84f5cd2bab2a8ac92d36b26f4783b
MineClever <36216195+MineClever@users.noreply.github.com>
2022-05-24 03:05:57 +0800

    # fix script

--- move_to_leanroot.bat
+++ move_to_leanroot.bat
@@ -2,19 +2,30 @@
 chcp 65001
 cls
 
-::input file
-
-set "cur=%~dp0"
-set "fbxFile=%~f1"
-pushd %cur%
-set "envPath=%cur%env\python39"
-set "scriptPath=%cur%fbxScript"
-
 :: add to sysPath
-set path=%SystemRoot%;%path%
-set path=%SystemRoot%\system32;%path%
+set "path=%SystemRoot%;%path%"
+set "path=%SystemRoot%\system32;%path%"
 set "path=%envPath%;%path%"
 set "path=%envPath%\Scripts;%path%"
 
+:: input file
+set "cur=%~dp0"
+set "fbxFile=%~f1"
+pushd %cur%
+
+set "scriptPath=%cur%fbxScript"
+
+:: check python env
+set "pythonEnvPath=%cur%env\python39"
+if exist %pythonEnvPath% do (
+    set "pythonExec=%pythonEnvPath%\python.exe"
+) else (
+    set "pythonExec=python.exe"
+)
+
 :: using script
-%envPath%\python.exe %scriptPath%\move_back_lean_root.py %fbxFile%
+set "scriptName=move_back_lean_root.py"
+%pythonExec% %scriptPath%\%scriptName% %fbxFile%
+
+pause
+:EOF
